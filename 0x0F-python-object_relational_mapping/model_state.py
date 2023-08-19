140b4142b7bf3a0e66185ea272cf960e604dfa26
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-19 13:02:56 +0200

    added class-doc

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+      """Represents a city for a MySQL database."""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
