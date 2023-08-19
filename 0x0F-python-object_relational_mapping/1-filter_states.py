1c51707cd158fbde92520cdec27b62f92bc7d1b7
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-19 14:15:10 +0200

    added main func

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+"""This module contains a script that lists start with N from hbtn_0e_0_usa"""
+
+
 import MySQLdb
 import sys
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+
+if __name__ == "__main__":
+  conn = MySQLdb.connect(
+                      host="localhost",
+                      port=3306,
+                      user=sys.argv[1],
+                      passwd=sys.argv[2],
+                      db=sys.argv[3],
+                      charset="utf8"
+                          )
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+  query_rows = cur.fetchall()
+  for row in query_rows:
+      print(row)
+  cur.close()
+  conn.close()
