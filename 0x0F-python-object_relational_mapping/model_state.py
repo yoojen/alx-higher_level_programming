b11f3faf01dee4b698e0190fa1cf8f4c1e6f335c
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-19 13:11:37 +0200

    updated

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,40 +1,20 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""this model creates State class
+      and connect to state table from MySQL
+"""
+
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-
-    __tablename__ (str): The name of the MySQL table to store States.
-    id (sqlalchemy.Integer): The state's id.
-    name (sqlalchemy.String): The state's name.
-    """
-    __tablename__ = "states"
+      """Represents a city for a MySQL database.
+            args: -> id --- name
+      """
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-#!/usr/bin/python3
-# """this model creates State class
-#       and connect to state table from MySQL
-# """
-
-
-# from sqlalchemy import Column, Integer, String
-# from sqlalchemy.ext.declarative import declarative_base
-
-
-# Base = declarative_base()
-
-
-# class State(Base):
-#       """Represents a city for a MySQL database.
-#             args: -> id --- name
-#       """
-#     __tablename__ = 'states'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(128), nullable=False)
