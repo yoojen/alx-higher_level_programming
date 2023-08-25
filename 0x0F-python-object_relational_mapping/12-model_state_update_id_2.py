98231205df2845a18bc3c98288b7b5962cb2469a
Eugene <140322568+yoojen@users.noreply.github.com>
2023-08-25 16:48:26 +0200

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ script that changes the name of a State object from the database hbtn_0e_6_usa"""
+
+
 import MySQLdb
 from model_state import Base, State
 from sqlalchemy import create _engine
 from sqlalchemy.ext.declarative import declarative_base
+
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
