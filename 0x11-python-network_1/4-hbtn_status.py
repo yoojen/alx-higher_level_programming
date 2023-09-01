135480093c4934ec1e6e0dea76824c11ffd91a71
Eugene <140322568+yoojen@users.noreply.github.com>
2023-09-01 20:10:33 +0200

    changed name

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,7 @@ fetch url using request package
 if __name__ == '__main__':
     import requests
 
-    my_url = "https://intranet.hbtn.io/status"
-    html = requests.get(my_url)
+    contents = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(html.text.__class__))
-    print("\t- content: {}".format(html.text))
+    print("\t- type: {}".format(contents.text.__class__))
+    print("\t- content: {}".format(contents.text))
